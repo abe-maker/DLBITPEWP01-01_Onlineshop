f1b87e88784034c7da8db7286174efa66c0f60b8
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2026-06-11 14:41:55 +0200

    update

--- main.py
+++ main.py
@@ -10,31 +10,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# def main():
-#     db = DatabaseManager("shop_test.db")
-#     print("Datenbank erfolgreich initialisiert.")
-
-#     Auto = Product(
-#             id = None,
-#             name = "Porsche",
-#             description = "cool",
-#             price = 100,
-#             category_id = 1
-#     )
-    
-
-#     db.add_product(Auto)
-
-#     firstOrder = Order(
-#         id = None,
-#         user_id = 1,
-#         status = "offen"
-#     )
-#     firstOrder.add_item(Auto)
-
-#     db.add_order(firstOrder)
-
-# if __name__ == "__main__":
-#     main()
